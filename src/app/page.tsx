93e33f68d358378fa489abe398ce073c9c54d8a0
StephenChi-hi <nwikechisom77@gmail.com>
2024-03-23 11:10:24 +0100

    Admin in progress

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Dashboard from "../app/admin/dashboard/page"
+
+export default function Home() {
+  return (
+    <main className="">
+    <Dashboard />
+    </main>
+  )
+}

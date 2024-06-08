cfa187be52e9972e0e59c122ad3944cf52d8020e
webspirrepro <webspirre@gmail.com>
2024-06-08 06:23:16 +0100

    drag and drop upload working, UI in progress

--- src/app/dashboard/FileUpload.tsx
+++ src/app/dashboard/FileUpload.tsx
@@ -1,6 +1,7 @@
-"use client";
-
-import React, { useState, useRef, ChangeEvent } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import type { Accept } from "react-dropzone";
+import axios from "axios";
 
 interface FileUploadProps {
   label: string;
@@ -14,44 +15,54 @@ const FileUpload: React.FC<FileUploadProps> = ({
   onFileChange,
 }) => {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [files, setFiles] = useState<File[]>([]);
 
-  const handleFileInputChange = async (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    const files = event.target.files;
-    if (!files) return;
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      setFiles(acceptedFiles);
+      if (acceptedFiles.length > 0) {
+        const file = acceptedFiles[0];
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImagePreview(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+        onFileChange(file, label, filename);
+      }
+    },
+    [label, filename, onFileChange]
+  );
 
-    const file = files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagePreview(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-      onFileChange(file, label, filename);
-      console.log("file content ", file);
-    }
-  };
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".jpeg", ".png", ".jpg"],
+    },
+  });
 
-  const handleUploadClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
+  useEffect(() => {
+    // Revoke the data uris to avoid memory leaks
+    // @ts-ignore
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
   return (
     <div className="flex flex-col w-full py-8 text-slate-700">
       <label htmlFor={label}>{label}</label>
       <div
+        {...getRootProps({ className: "dropzone" })}
         className="p-4 flex w-full border-2 border-gray-300 rounded-md justify-between items-center cursor-pointer"
-        onClick={handleUploadClick}
       >
-        <p className="text-[gray]">Upload</p>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
         {imagePreview ? (
           <img
             src={imagePreview}
-            alt="Uploaded preview"
+            alt="Uploaded preview" 
             height={40}
             width={40}
             className="cursor-not-allowed"
@@ -66,14 +77,6 @@ const FileUpload: React.FC<FileUploadProps> = ({
           />
         )}
       </div>
-      <input
-        type="file"
-        accept="image/*"
-        required
-        onChange={handleFileInputChange}
-        ref={fileInputRef}
-        style={{ display: "none" }}
-      />
     </div>
   );
 };
